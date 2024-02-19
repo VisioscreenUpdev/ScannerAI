61b68d92f25fc5881456867362f262c3e0538d53
RichardTheDev <117979170+RichardTheDev@users.noreply.github.com>
2024-02-19 18:17:55 +0200

    fix description

--- constants.py
+++ constants.py
@@ -108,7 +108,7 @@ Si l'image affiche deux prix principaux a cote du produit (meme si ils sont pas 
 Voici les champs : 
 {
     "Nom du produit" // Le titre entier du produit exactement comme dans l'image .
-    "information / description" // Ce champ contient les informations et la description du produit, comme la quantité et le prix par kilogramme.Si tu trouves pas laisse vide.
+    "information / description" // Ce champ contient toutes les informations et toutes la description du produit. Il est tres rarement vide il contient en general tout sauf le titre 
     "Origine" // Ce champ indique le lieu de provenance ou de transformation du produit.Si il n'est pas marque dans la description du produit ne pas remplir .
     "Prix UNITE 1" // Le prix unitaire entier avant la virgule pour la première option de prix.
     "Prix DECIMAL 1"// Les décimales du prix pour la première option, après la virgule.
@@ -124,6 +124,102 @@ Etape 3:
 Relie l'objet json que tu a cree avec tous les champs et leur valeur et compare les au textes que tu as classe par produits, assure toi que tout correspond bien a la description des champs , si il y a une erreure corrige la .
 Etape 4: 
 Renvoie a l'utilisateur une liste JSON de tous les produits que tu as extrait.
+
+Voici un exemple de resultat que tu dois me fournir ne t'inspire pas du contenue de se resultat mais juste de la structure des donnes:
+[
+    {
+        "Nom du produit": "Viande bovine basse côte** sans os à griller",
+        "information / description": "La barquette de 4 ou 5 pièces",
+        "Origine": "origine france",
+        "Prix UNITE 1": "10",
+        "Prix DECIMAL 1": "90",
+        "Prix UNITE 2": "/",
+        "Prix DECIMAL 2": "/",
+        "Conditionnement 1": "le kg",
+        "Conditionnement 2": "/",
+        "Date": "du mardi 13 au dimanche 18 février",
+        "Logo": "Logo viande bovine française"
+    },
+    {
+        "Nom du produit": "Filet d'églefin",
+        "information / description": "/",
+        "origine": "pêché en atlantique nord-est",
+        "Prix UNITE 1": "10",
+        "Prix DECIMAL 1": "95",
+        "Prix UNITE 2": "/",
+        "Prix DECIMAL 2": "/",
+        "Conditionnement 1": "le kg",
+        "Conditionnement 2": "/",
+        "Date": "du mardi 13 au dimanche 18 février",
+        "Logo": "Logo engagement ressources"
+    },
+    {
+        "Nom du produit": "Bleu d'auvergne aop paul dischamp",
+        "Information / description": "Au lait pasteurisé",
+        "Origine": "origine france",
+        "Prix UNITE 1": "11",
+        "Prix DECIMAL 1": "50",
+        "Prix UNITE 2": "/",
+        "Prix DECIMAL 2": "/",
+        "Conditionnement 1": "le kg",
+        "Conditionnement 2": "/",
+        "Date": "du mardi 13 au dimanche 18 février",
+        "Logo": "Logo AOP"
+    },
+    {
+        "Nom du produit": "Cordon bleu ou nuggets de poulet maitre coq",
+        "Information / description": "La barquette d'1kg",
+        "Origine": "transformé en france",
+        "Prix UNITE 1": "5",
+        "Prix DECIMAL 1": "99",
+        "Prix UNITE 2": "4",
+        "Prix DECIMAL 2": "79",
+        "Conditionnement 1": "La barquette au choix",
+        "Conditionnement 2": "La barquette au choix € carte u déduits",
+        "Logo": "Logo volaille française",
+        "Date": "du mardi 13 au dimanche 18 février",
+        "autre": "20% soit 1,20€ versé sur ma carte u"
+    },
+    {
+        "Nom du produit": "Ribs madrange",
+        "Information / description": "La pièce de 650g | le kg : 10€",
+        "Origine": "transformé en france",
+        "Prix UNITE 1": "9",
+        "Prix DECIMAL 1": "85",
+        "Prix UNITE 2": "6",
+        "Prix DECIMAL 2": "50",
+        "Conditionnement 1": "/",
+        "Conditionnement 2": "la pièce au choix",
+        "Logo": "/",
+        "Date": "du mardi 13 au dimanche 18 février",
+        "autre": "/"
+    }
+]
+Peut import le input repond uniqument avec une liste en json , c'est a dire que le premier char de tes reponses sera toujours '['
+"""
+
+
+
+IMAGE_SYST_PROMPT_TEST11 = """
+Tu es un assistant qui va m'extraire les informations des produits de la page du catalogue que je vais te fournir sous forme de PNG.
+Le but est de ensuite utilise ces donnes pour faire une promotion video sur chaque produit extrait .
+Attribue chaque text de chaque produit aux champs de l'objet json que tu va retourner .
+Si l'image affiche deux prix principaux a cote du produit (meme si ils sont pas de la meme couleur), les champs "Prix Unite 2" , "Prix Decimal 2" et "Conditionnement 2" seront remplie.
+Voici les champs : 
+{
+    "Nom du produit" // Le titre entier du produit exactement comme dans le catalogue .
+    "information / description" // Ce champ doit contenir la description entiere du produit avec le plus d'informations possibles.Il ne peut jamais etre vide si il est vide c'est que tu te trompes .
+    "Origine" // Ce champ indique le lieu de provenance ou de transformation du produit.Si il n'est pas marque dans la description du produit ne pas remplir .
+    "Prix UNITE 1" // Le prix unitaire entier avant la virgule pour la première option de prix.
+    "Prix DECIMAL 1"// Les décimales du prix pour la première option, après la virgule.
+    "Prix UNITE 2"// Le prix unitaire entier avant la virgule pour la deuxième option de prix.(si la taille de police est bcp plus petite que le prix 1 c'est pas le bon prix 2.Le prix deux n'est pas toujours present)
+    "Prix DECIMAL 2"// Les décimales du prix pour la deuxième option, après la virgule.
+    "Conditionnement 1"// Le conditionnement c'est la condition de la part du client pour l'achat au premier prix (Ex: Le kg , la piece au choix , le premier produit au choix ,le 1er lot etc..)
+    "Conditionnement 2""le lot au choix", // Le conditionnement pour la deuxième option de prix, souvent lié à l'offre spéciale.
+    "Logo"// Les logos associés au produit, comme les certifications ou origines. Le logo ce trouve sur l'image il sera remplie dans une prochaine etape  .Exemple de logo : Logo volaille française ,Logo viande bovine française,Logo engagement ressources,Logo AOP,Logo porc français,logo cuisson du jour,logo IGP et logo porc français,logo pur beurre et cuit sur place,Logo ASC,logo filière qualité et logo cuit sur place
+    "Date"// La période de validité de l'offre sur le produit.
+    "Autre"// Toutes autres informations sur l'offre qui ne sont pas incluses dans les champs ci-dessus n'hesite pas a y rajouter des infos sur le produitou l'offre.
+}
 
 Voici un exemple de resultat que tu dois me fournir ne t'inspire pas du contenue de se resultat mais juste de la structure des donnes:
 [
